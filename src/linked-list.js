b6cc8f794a918d1fd0fc793244c9b71a1f20fe76
Tatiana <ganzhinovat@yandex.ru>
2020-06-28 16:39:57 +0300

    Add solution of task in linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,93 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+        this._nodes = []; //в массиве хранятся все node (узлы), что были созданы по порядку
+    }
 
-    append(data) {}
+    append(data) {
+        var newNode = new Node(data, null, null);
 
-    head() {}
+        if (this.length == 0) {
+           this._head = newNode; //при длине 0 head и tail совпадают
+        }
 
-    tail() {}
+        this._tail = newNode;
 
-    at(index) {}
+        this._nodes.push(newNode); //добавляем в массив
 
-    insertAt(index, data) {}
+        this.length++; 
 
-    isEmpty() {}
+        return this; //для последней задачи
 
-    clear() {}
 
-    deleteAt(index) {}
+    }
 
-    reverse() {}
+    head() {
+        if (this._head == null) {
+            return null;
+        }
+        return this._head.data;
+    }
 
-    indexOf(data) {}
+    tail() {
+        if (this._tail == null) {
+            return null;
+        }
+        return this._tail.data;
+    }
+
+    at(index) {
+       return this._nodes[index].data; //возвращаем данные i-той node (узла)
+    }
+
+    insertAt(index, data) {
+       var newNode = new Node(data, null, null); //вставляем не просто data, а создаем новый узел
+       this._nodes.splice(index, 0, newNode);
+       this.length++; 
+       return this; //для последней задачи
+    }
+
+    isEmpty() {
+        if (this.length === 0) {
+            return true;
+        }
+        return false;
+    }
+
+    clear() {
+        this._nodes.splice(0, this.length);
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+        return this; //для последней задачи
+
+    }
+
+    deleteAt(index) {
+        this._nodes.splice(index, 1);
+        this.length--;
+        return this; //для последней задачи
+    }
+
+    reverse() {
+        this._nodes.reverse();
+        this._head = this._nodes[0];
+        this._tail = this._nodes[this.length-1];
+        return this; //для последней задачи
+    }
+
+    indexOf(data) {
+        for (let i = 0; i < this.length; i++) {
+            if (this._nodes[i].data === data) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
